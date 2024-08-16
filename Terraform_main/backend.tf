f2f373073075baaffda53c4187acf08e32cefcdf
Kene Josephat <josephatkene@gmail.com>
2024-08-16 13:07:46 +0100

    infrastructure provisioning amend4

--- Terraform_main/backend.tf
+++ Terraform_main/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "jovals_Bucket_for_capstone16_08_1960"
+    bucket         = "jovals_Bucket_capstone_93"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-lockfile-for-capstone"
+    dynamodb_table = "terraform-lockfile-for-capstone_93"
     encrypt = true
   }
 }
