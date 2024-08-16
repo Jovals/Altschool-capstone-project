63d493ffbd07dc3e04e8517554a18af0923f3925
Kene Josephat <josephatkene@gmail.com>
2024-08-16 12:56:21 +0100

    infrastructure provisioning amend2

--- Terraform_main/backend.tf
+++ Terraform_main/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "sock-bucket-98"
+    bucket         = "jovals-bucket-98"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lockfile"
